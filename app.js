2da63708073c9a9786fa5a71de7a93390e3126c8
Emnolope <emmanuelnlopez+15@gmail.com>
2022-11-04 04:39:07 +0000

    Never tested

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 //setting middleware
-var __dirname='./';
+var __dirname='./day2/';
 var safety='';
 app.use(express.static(__dirname + safety)); //Serves resources from public folderno
 app.listen(8080);
